ba9f21f403209fa230c9c687bc7b3201fe124721
Daniel Silva <dfilipes@protonmail.com>
2017-05-08 01:16:14 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/podcasts/web/channels/user_socket.ex
+++ lib/podcasts/web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Podcasts.Web.UserSocket do
   # channel "room:*", Podcasts.Web.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
