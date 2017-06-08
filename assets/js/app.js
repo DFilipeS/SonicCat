f4f5e9f4d436ea0bd585e991fd78138de67a8a01
Daniel Silva <dfilipes@protonmail.com>
2017-06-08 23:23:41 +0100

    Added node dependencies like React, Redux and others. Added scafolding code for Redux and react-router.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,25 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createLogger } from 'redux-logger';
+
+import reducers from './reducers'
+import IndexComponent from './components/index';
 import 'phoenix_html';
 
-render(<div>Another</div>, document.getElementById('app'));
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers, reduxDevTools)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={IndexComponent} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('app')
+);
